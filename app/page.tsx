abf975d891ebb447f613ed3c1b20b80df5a5a46b
yoooonbong <a1056719@gmail.com>
2024-12-05 18:15:45 +0900

    ✨ 로그인 기능 구현(회원가입 후 자동으로)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import LogoutButton from "components/logout-button";
+import { createServerSupabaseClient } from "utils/supabse/server";
 
 export const metadata = {
   title: "Inflearngram",
   description: "Instagram clone project",
 };
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-2">
-      <h1 className="text-xl font-bold">Welcome {"lopun.jh"}!</h1>
+      <h1 className="text-xl font-bold">
+        Welcome {session?.user?.email?.split("@")?.[0]}!
+      </h1>
       <LogoutButton />
     </main>
   );
