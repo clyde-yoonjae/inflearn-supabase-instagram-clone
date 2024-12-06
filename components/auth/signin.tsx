1a3486a6989117c849b97e51251a3548dffb79d6
yoooonbong <a1056719@gmail.com>
2024-12-06 16:36:07 +0900

    ✨ 로그인 기능 구현

--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabse/clients";
 export default function SignIn({ setView }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const supabase = createBrowserSupabaseClient();
+
+  const signInMutation = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (data) {
+        console.log(data);
+      }
+      if (error) {
+        alert(error.message);
+      }
+    },
+  });
   return (
     <div className="flex flex-col gap-4">
       <div className="flex w-full max-w-lg flex-col items-center justify-center gap-2 border border-gray-400 bg-white px-10 pb-6 pt-10">
@@ -23,8 +43,10 @@ export default function SignIn({ setView }) {
         />
         <Button
           onClick={() => {
-            console.log("signin");
+            signInMutation.mutate();
           }}
+          loading={signInMutation.isPending}
+          disabled={signInMutation.isPending}
           color="light-blue"
           className="text-md w-full py-1"
         >
@@ -35,7 +57,7 @@ export default function SignIn({ setView }) {
         아직 계정이 없으신가요?{" "}
         <button
           className="font-bold text-light-blue-600"
-          onClick={() => setView("SIGNIN")}
+          onClick={() => setView("SIGNUP")}
         >
           가입하기
         </button>
