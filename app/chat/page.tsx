23678b9232f471cdfbb62bf609646cd3ccb1370d
clyde-yoonjae <leewh77777@gmail.com>
2024-12-10 19:43:29 +0900

    💄 ChatPage컴포넌트 디자인셋팅

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,11 @@
+import ChatPeopleList from "components/chat/ChatPeopleList";
+import ChatScreen from "components/chat/ChatScreen";
+import Person from "components/chat/Person";
 export default function ChatPage() {
-  return <div className="flex h-screen w-full justify-center">Chat Page</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <ChatPeopleList />
+      <ChatScreen />
+    </div>
+  );
 }
