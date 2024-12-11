ec3e33e4238eb81560a8b23c83446f16bbf017d1
yoooonbong <a1056719@gmail.com>
2024-12-11 17:44:06 +0900

    ✨ SendMessage 서버액션 구현

--- actions/chatActions.ts
+++ actions/chatActions.ts
@@ -1,5 +1,8 @@
-"use server";
-import { createServerSupabaseAdminClient } from "utils/supabase/server";
+'use server';
+import {
+  createServerSupabaseAdminClient,
+  createServerSupabaseClient,
+} from 'utils/supabase/server';
 export async function getAllUsers() {
   const supabase = await createServerSupabaseAdminClient();
   const { data, error } = await supabase.auth.admin.listUsers();
@@ -15,4 +18,31 @@ export async function getUserById(userId) {
     return null;
   }
   return data.user;
+}
+
+export async function sendMessage({ message, chatuserId }) {
+  const supabase = await createServerSupabaseClient();
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error && !session.user) {
+    throw new Error('User is not authenticated');
+  }
+
+  const { data, error: sendMessageError } = await supabase
+    .from('message')
+    .insert({
+      message,
+      receiver: chatuserId,
+      sender: session.user.id,
+    });
+
+  if (sendMessageError) {
+    throw new Error(sendMessageError.message);
+  }
+
+  return data;
 }
